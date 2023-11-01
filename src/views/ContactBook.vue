5c8fd0f52f62f14ad95e913427ae32c0cc53f724
Kinji <linhqwertyuiop2gmail.com>
2023-11-01 14:14:33 +0700

    Tao trang them moi mot lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -14,7 +14,7 @@
       <p v-else>Không có liên hệ nào.</p>
       <div class="mt-3 row justify-content-around align-items-center">
         <button class="btn btn-sm btn-primary" @click="refreshList()">
-          <i class="fas fa-redo"></i> Làm mới
+          <i class="fa-solid fa-rotate"></i> Làm mới
         </button>
         <button class="btn btn-sm btn-success" @click="goToAddContact">
           <i class="fas fa-plus"></i> Thêm mới
